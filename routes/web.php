cce259712525071b4ef88790b27794cc60740838
ahmed yasser <ahmedyasser@alAiralasbahmed.home>
2025-02-15 22:58:47 +0200

    added mcamra and database

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale() . '/dashboard',
+        'as' => 'dashboard.',
+        'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath']
+    ],
+    function () {
+                
+    }
+);
